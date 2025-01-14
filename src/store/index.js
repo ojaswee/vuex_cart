1d73f7cc48558844d54912c56ea70132a071e1c6
Ojaswee Dhungana <odhungana@itsmail.ca>
2025-01-14 15:14:56 -0800

    {initial_commit_message}

---
+++ src/store/index.js
@@ -0,0 +1,14 @@
+import { createStore } from 'vuex';
+
+import productsModule from './modules/products';
+import cartModule from './modules/cart';
+
+const store = createStore({
+	modules: {
+		prods: productsModule,
+		cart: cartModule
+	}
+
+});
+
+export default store;
